ffa5243a17ec17adc95a4c6c55eee7de4454b039
George Lopez <50560354+GeorgeChristopherLopez@users.noreply.github.com>
2019-10-04 12:28:02 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,72 @@
 import React, { Component } from 'react';
+
+import {  Link } from 'react-router-dom';
+import styled from 'styled-components';
 import { Icon } from 'react-icons-kit';
 import { ic_search } from 'react-icons-kit/md/ic_search';
-import { Link } from 'react-router-dom';
+// MEDIA QUERY
+import { generateMedia } from 'styled-media-query';
+
+
+
 
 
 class Header extends Component {
-    render(){
+    render() {
         return (
-            <header className="App-header" >
-                <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+          
 
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <Link className="navbar-brand" to="\">Marvel</Link>
-                    <div className="offset-1"></div>
+            <nav style={headerStyle} className="navbar navbar-expand-lg navbar-dark header-container px-3">
 
+                <Link className="navbar-brand col-xs-12 col-md-5" to="/">FLEX CENTRAL</Link>
 
-                    <div className="collapse navbar-collapse" id="navbarToggler">
-                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                            <li className="nav-item active">
-                                <Link className="nav-link" to="\">Home <span className="sr-only">(current)</span></Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="\">Characters</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="\">Comics</Link>
-                            </li>
-                        </ul> 
-                    </div>
-
-                    <div>
-                        <form className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-2 header-input" type="search" placeholder="Search" aria-label="Search" /><Icon style={headerIconStyle} size={32} icon={ic_search} />
-                         </form>
-                    </div>
-                    
-</nav>
+                <div style={{ display: 'flex' }} className="search-bar col-md-4 col-xs-12">
+                    {this.props.show ? <input className="search-box" type="text" onChange={this.props.changed} /> : null}
+                    {!this.props.show ? <Icon size={32} icon={ic_search} onClick={this.props.clicked} /> : null}
+                    {this.props.show ? <button style={btnStyle}><Link to="/search"> Search</Link></button> : null}
+                </div>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                
-            </header>
-
-
-        );
-
-
+                <div className="collapse navbar-collapse col-xs-12  col-md-3" id="navbarNav">
+                    <ul className="navbar-nav text-center">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Login</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+          
+           
+      
+            );
     }
-    
+
+
 }
+
+
 
 export default Header;
 
-
-const headerIconStyle = {
-    color: 'white'
-
+const headerStyle = {
+    background: 'var(--main-color)',
+    color: 'var(--main-light)',
+    margin: '0rem'
+    
 }
 
+
+const btnStyle = {
+    background: 'var(--main-deep-dark)',
+    'font-weight': 'bold',
+    border: 'none',
+    padding: '.5rem 1rem'
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const key = 'f56610dee3836c9b80964e9be6d563c2';
+/* https://api.themoviedb.org/3/movie/550?api_key=f56610dee3836c9b80964e9be6d563c2 */
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
